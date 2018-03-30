32b392cc0ea4be072ed575de47ce9f3f61112e6c
Phil Werner <philwerner@shaw.ca>
2018-03-30 23:15:40 +0000

    Added clearing of text area upon new tweet / cleaned up the code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,59 +6,139 @@
 
 $(document).ready(function() {
 
-  $('.tweet').hover(function(){
+  /* The escape function takes in a string and returns one that has had things like
+  <script> etc converted to literal text.  This prevents malicious users from enterting
+  scipts into text input areas. */
 
-    $('.userName').toggleClass("hovered");
+  function escape(str) {
 
-  });
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
-  // $('#nav-bar .compose').on('mouseover', function(event) {
+  /* The function modifyTimeStamp takes in a timeStamp called tweetTime and returns
+  a string that contains the amount of time passed in hours, or days if it has been
+  at least a day. */
 
-  //   event.preventDefault();
+  function modifyTimeStamp(tweetTime) {
 
-  // })
+    let outputString = "";
 
+    let now = new Date();
+    let currentTime = now.getTime();
+    let modifiedTime = currentTime - tweetTime;
+    modifiedTime = modifiedTime / 1000;
+    modifiedTime = modifiedTime / 60;
+    let timeInHours = modifiedTime / 60;
+    let timeInDays = timeInHours / 24;
+
+    if (Math.floor(timeInDays) === 0) {
+      if (Math.round(timeInHours) === 1) {
+        outputString = Math.round(timeInHours) + " hour ago";
+      } else {
+          outputString = Math.round(timeInHours) + " hours ago";
+      }
+    } else {
+      if (Math.round(timeInDays) === 1) {
+          outputString = Math.round(timeInDays) + " day ago";
+      } else {
+          outputString = Math.round(timeInDays) + " days ago";
+      }
+    }
+    return outputString;
+  }
+
+  /* The function createTweetElement takes in a tweet (json format) and returns an html
+  element ($tweet) that has had all the data from the tweet converted into the appropriate
+  html elements. */
+
+  function createTweetElement(tweet) {
+
+    let $tweet = $("<article>").addClass("tweet");
+    let $tweetHead = $("<div>").addClass("tweetHeader");
+    let $userName = $("<p>").addClass("userName").html(tweet["user"]["name"]);
+    let $avatar = $("<div>").addClass("avatar").html('<img src=' + tweet["user"]["avatars"]["regular"] + '>');
+    let $handle = $("<div>").addClass("handle").html(tweet["user"]["handle"]);
+    let $tweetContent = $("<p>").addClass("someTweet").html(escape(tweet["content"]["text"]));
+    let $tweetDate = $("<footer>").addClass("footer").html(modifyTimeStamp(tweet["created_at"]));
+    let $line = $("<section>").html("<hr>");
+
+    $tweetHead.append($avatar);
+    $tweetHead.append($userName);
+    $tweetHead.append($handle);
+    $tweet.append($tweetHead);
+    $tweet.append($tweetContent);
+    $tweet.append($line);
+    $tweet.append($tweetDate);
+
+    return $tweet;
+  }
+
+/* The function renderTweets takes in an array of tweets (JSON format) and calls
+createTweetElement for each tweet in the array, then appends each tweet to the
+main Container.  */
+
+  function renderTweets(tweets) {
+    for (let i = 0; i < tweets.length; i++) {
+      let $currentTweet = createTweetElement(tweets[i]);
+      $("#mainContainer").append($currentTweet);
+    }
+  }
+/* The function loadTweets uses ajax to load all the tweets from the mongo database */
+
+  function loadTweets() {
+
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function (tweets) {
+        renderTweets(tweets);
+      }
+    });
+  }
+
+  // $('.tweet').hover(function(){
+
+  //   $('.userName').toggleClass("hovered");
+  // });
+
+  let input = $('#textTweet');
 
   $('#nav-bar .pencil').on('click', function() {
 
-    //console.log("!!!");
     if ( $( '.new-tweet').is (":hidden") ) {
       $( '.new-tweet').slideDown( "slow" );
+      input.focus();
     } else {
-      $( ".new-tweet").hide();
-
+      $( ".new-tweet").slideUp( "slow" );
     }
-
   });
 
   $('#nav-bar .compose').on('click', function() {
 
-   // console.log("!!!");
     if ( $( '.new-tweet').is (":hidden") ) {
       $( '.new-tweet').slideDown( "slow" );
+      input.focus();
     } else {
-      $( ".new-tweet").hide();
-
+      $( ".new-tweet").slideUp( "slow ");
     }
-
   });
 
 
   $('.new-tweet form').on('submit', function(event) {
 
     event.preventDefault();
-
+    let inputText = $("#textTweet");
     let inputString = $("#textTweet").val();
     let inputSerial = $("#textTweet").serialize();
 
     if (inputString.length === 0) {
-
       alert("Error:  Your tweet is empty!  Please add some text to your tweet :)");
       return;
     }
 
     if (inputString.length > 140) {
-
       alert("Error:  Your tweet is too long!  Please keep your tweet to 140 characters or less!");
       return;
     }
@@ -70,80 +150,19 @@ $(document).ready(function() {
       data: inputSerial,
       success: function (){
 
-
         $.ajax({
-         url: '/tweets',
-         method: 'GET',
-         success: function (tweets) {
-
-        console.log(tweets.length -1);
-
-        let $newTweet = createTweetElement(tweets[tweets.length - 1]);
-           //console.log($newTweet);
-           $("#mainContainer").append($newTweet);
-         }
-       })
+          url: '/tweets',
+          method: 'GET',
+          success: function (tweets) {
+            let $newTweet = createTweetElement(tweets[tweets.length - 1]);
+            $("#mainContainer").append($newTweet);
+          }
+        })
       }
     })
+    inputText.val("");
+    $('#counter').html(140);
   });
 
-
-  function escape(str) {
-
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  }
-
-  function createTweetElement(tweet) {
-
-    let $tweet = $("<article>").addClass("tweet");
-    let $tweetHead = $("<div>").addClass("tweetHeader");
-    let $userName = $("<p>").addClass("userName").html(tweet["user"]["name"]);
-    let $tweetContent = $("<section>").addClass("someTweet").html(escape(tweet["content"]["text"]));
-    let $tweetDate = $("<footer>").addClass("footer").html(tweet["created_at"]);
-    let $line = $("<section>").html("<hr>");
-
-
-    $tweetHead.append($userName);
-    $tweet.append($tweetHead);
-
-    $tweet.append($tweetContent);
-    $tweet.append($line);
-    $tweet.append($tweetDate);
-
-    return $tweet;
-
-  }
-
-  function renderTweets(tweets) {
-
-    for (let i = 0; i < tweets.length; i++) {
-
-      let $currentTweet = createTweetElement(tweets[i]);
-
-      $("#mainContainer").append($currentTweet);
-
-    }
-
-  }
-
-  function loadTweets() {
-
-    $.ajax({
-      url: '/tweets',
-      method: 'GET',
-      success: function (tweets) {
-        //console.log(tweets);
-        renderTweets(tweets);
-      }
-
-    });
-  }
-
   loadTweets();
-
 });
-
-
-
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,17 @@
 $(document).ready(function() {
-  //console.log("This occurred.");
 
   $('textarea').on('keyup', function(){
 
     let textLength = $(this).val().length;
-    //console.log(textLength);
     let textRemaining = 140 - textLength;
 
-    //console.log($(this).siblings('.counter').val());
-    //console.log($('.counter')[0].outerText);
-    //$('.counter')[0].outerText = textRemaining;
-
       if (textRemaining < 0) {
-      //  console.log("!!!");
         $('#counter').addClass('belowZero');
-
       }
 
-
-    //$(this).siblings('.counter')[0].html(textRemaining);
+      if (textRemaining >= 0) {
+        $('#counter').removeClass('belowZero');
+      }
     $('#counter').html(textRemaining);
-
-
   });
 });
